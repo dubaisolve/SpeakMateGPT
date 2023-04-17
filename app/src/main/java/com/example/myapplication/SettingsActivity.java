5223eb6e4be7b8bcfb2efad26e3a52673b0e7c61
north <northstreem@gmail.com>
2023-04-17 16:21:53 -0700

    commiting new changes with gpt 3 history keeping

--- app/src/main/java/com/example/myapplication/SettingsActivity.java
+++ app/src/main/java/com/example/myapplication/SettingsActivity.java
@@ -1,15 +1,21 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -42,6 +48,29 @@ public class SettingsActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        // Initialize RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        List<Voice> voices = new ArrayList<>();
+        voices.add(new Voice("21m00Tcm4TlvDq8ikWAM", "Rachel", "https://storage.googleapis.com/eleven-public-prod/premade/voices/21m00Tcm4TlvDq8ikWAM/6edb9076-c3e4-420c-b6ab-11d43fe341c8.mp3"));
+        voices.add(new Voice("AZnzlk1XvdvUeBnXmlld", "Domi", "https://storage.googleapis.com/eleven-public-prod/premade/voices/AZnzlk1XvdvUeBnXmlld/69c5373f-0dc2-4efd-9232-a0140182c0a9.mp3"));
+        voices.add(new Voice("EXAVITQu4vr4xnSDxMaL", "Bella", "https://storage.googleapis.com/eleven-public-prod/premade/voices/EXAVITQu4vr4xnSDxMaL/04365bce-98cc-4e99-9f10-56b60680cda9.mp3"));
+        voices.add(new Voice("ErXwobaYiN019PkySvjV", "Antoni", "https://storage.googleapis.com/eleven-public-prod/premade/voices/ErXwobaYiN019PkySvjV/38d8f8f0-1122-4333-b323-0b87478d506a.mp3"));
+        voices.add(new Voice("MF3mGyEYCl7XYWbV9V6O", "Elli", "https://storage.googleapis.com/eleven-public-prod/premade/voices/MF3mGyEYCl7XYWbV9V6O/f9fd64c3-5d62-45cd-b0dc-ad722ee3284e.mp3"));
+        voices.add(new Voice("TxGEqnHWrfWFTfGW9XjX", "Josh", "https://storage.googleapis.com/eleven-public-prod/premade/voices/TxGEqnHWrfWFTfGW9XjX/c6c80dcd-5fe5-4a4c-a74c-b3fec4c62c67.mp3"));
+        voices.add(new Voice("VR6AewLTigWG4xSOukaG", "Arnold", "https://storage.googleapis.com/eleven-public-prod/premade/voices/VR6AewLTigWG4xSOukaG/66e83dc2-6543-4897-9283-e028ac5ae4aa.mp3"));
+
+
+        VoiceAdapter.OnItemClickListener onItemClickListener = previewUrl -> {
+            // Handle the click event, e.g., open the link in a browser
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(previewUrl));
+            startActivity(browserIntent);
+        };
+
+        VoiceAdapter adapter = new VoiceAdapter(voices, onItemClickListener);
+        recyclerView.setAdapter(adapter);
     }
 
     private void loadData() {
---
+++ app/src/main/java/com/example/myapplication/VoiceAdapter.java
@@ -0,0 +1,49 @@
+package com.example.myapplication;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class VoiceAdapter extends RecyclerView.Adapter<VoiceAdapter.ViewHolder> {
+
+    private List<Voice> voices;
+    private OnItemClickListener onItemClickListener;
+    public interface OnItemClickListener {
+        void onItemClick(String previewUrl);
+    }
+    public VoiceAdapter(List<Voice> voices, OnItemClickListener onItemClickListener) {
+        this.voices = voices;
+        this.onItemClickListener = onItemClickListener;
+
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.voice_item, parent, false);
+        return new ViewHolder(view);
+    }
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Voice voice = voices.get(position);
+        holder.voiceName.setText(voice.getName());
+        holder.itemView.setOnClickListener(v -> onItemClickListener.onItemClick(voice.getPreviewUrl()));
+        holder.voiceId.setText(voice.getVoiceId());
+    }
+    @Override
+    public int getItemCount() {
+        return voices.size();
+    }
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView voiceId;
+        TextView voiceName;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            voiceId = itemView.findViewById(R.id.voice_id);
+            voiceName = itemView.findViewById(R.id.voice_name);
+        }
+    }
+}
